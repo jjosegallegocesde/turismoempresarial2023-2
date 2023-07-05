824a295bc14e66f2acb0a2d49b3daaf75acb03d8
MlnCarlos <carlos_molina193@hotmail.com>
2023-07-05 11:55:49 -0500

    Se crea ejemplo de modificadores de acceso

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.modelos.Usuario;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer edad = 29;
+        //LOS OBJETOS SON VARIABLES, ESPECIALES
+        Universidad objetoUniversidad = new Universidad("Carlos");
+        Usuario objetoUsuario = new Usuario(25,"Carlos","1234",4,"ejemplo@gmail.com");
+
+        //CREO UN OBJETO PARA ACCEDER O MODIFICAR LOS ATRIBUTOS O METODOS DE ESE OBJETO
+        System.out.println(objetoUniversidad);
+        System.out.println(objetoUsuario);
     }
 }
